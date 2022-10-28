a8d116d5c259f8ea0f8872b2eddf11d6d859c3b4
Diego Morais <diegodev9722@gmail.com>
2022-10-28 03:23:08 -0300

    criada pagina de login

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,8 +1,34 @@
-
+import * as S from './style'
+import cozinheiraImage from '../../assets/imgs/cozinheiralogin.png'
 
 export const Login=()=>{
     return <>
-    login
+    <S.ContainerLogin>
+        <S.Container>
+          <S.ContainerDivision>
+          <div className="cx-form">
+              <S.Form >
+                <h3>Login</h3>
+                <p>Preencha com seus dados para entrar : </p>
+                <div className="cx-inputs">
+                    <input type="text" />
+                    <input type="email" />
+                    <input type="password" />
+                    <input type="submit" className='submit' value='Entrar' />
+                </div>
+
+
+
+              </S.Form>
+           </div>
+           <div className="cx-image">
+               <img src={cozinheiraImage} alt="" />
+           </div>
+          </S.ContainerDivision>
+        </S.Container>
+
+    </S.ContainerLogin>
+  
     
     
     </>
---
+++ src/Pages/Login/style.ts
@@ -0,0 +1,110 @@
+import styled from "styled-components";
+
+
+
+export const ContainerLogin=styled.div`
+background-color: #d4cc3b;
+display: flex;
+justify-content: center;
+padding:100px 0 ;
+`
+
+export const Container=styled.div`
+width: 80vw;
+display: flex;
+
+    @media screen  and (max-width:950px){
+        width: 100vw;
+        padding: 0 5px;
+    }
+`
+
+export const ContainerDivision=styled.div`
+    height: 100%;
+    display: flex;
+    width: 100%;
+    
+.cx-form{
+    width: 70%;
+}
+.cx-image{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+ height: 100%;
+    img{
+        height: 300px;
+    }
+}
+
+
+@media screen and (max-width:950px){
+    flex-direction: column;
+    .cx-image{
+        order: 1;
+     
+        img{
+            display: none;
+            height: 300px;
+            width: 300px;
+         }
+     }
+     .cx-form{
+        display: flex;
+        justify-content: center;
+       width: 100vw;
+      
+}
+}
+
+`
+
+export const Form=styled.form`
+background-color: #383835;
+border-radius: 13px;
+padding: 70px 0;
+color: #a8a11e;
+box-shadow: 0 0 5px #eee;
+
+h3{
+    padding-left: 100px;
+    font-size: 25px;
+}
+p{
+    margin: 30px 12px;
+}
+.cx-inputs{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    input{
+        width: 80%;
+        padding: 15px 6px;
+        margin: 10px 0;
+        border-radius: 5px;
+        background-color:#61615c;
+        border: none;
+        color: white;
+        &:focus{
+            transition:all ease 3s;
+            outline: 2px solid #Eeee;
+        }
+        
+    }
+    .submit{
+        background-color: #0b8c25;
+        &:hover{
+            transition:all ease 2s;
+            cursor: pointer;
+            background-color: #0d7522;
+        }
+    }
+}
+
+@media screen  and (max-width:950px){
+    width: 90%;
+}
+`
